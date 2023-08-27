8e1e38a115b3a8328e1064a01ccb7f8bc9457b50
praticodes <pratibha.thakur@mail.utoronto.ca>
2023-08-27 13:22:43 +0200

    Define TaleGraph and Character

--- graph.py
+++ graph.py
@@ -1,10 +1,12 @@
 """This file defines the graph data structure, nodes, edges, attributes, and relationships.
-The structure of the graph data structure in this file is largely drawn from the format promoted in the University of Toronto's 
-CSC111: Foundations of Computer Science II. I took this course in the Winter, 2023 Semester.
+The structures of the Graph and Node superclasses in this file are largely drawn from the format promoted in the
+University of Toronto's CSC111: Foundations of Computer Science II lectures.
+I took this course in the Winter, 2023 Semester.
 """
 
 from __future__ import annotations
 from typing import Any
+
 
 class _Node:
     """A node in a graph.
@@ -21,9 +23,9 @@ class _Node:
         self.item = item
         self.neighbours = neighbours
 
-  class Graph:
-    """A graph.
 
+class Graph:
+    """A graph.
     Representation Invariants:
     - all(item == self.nodes[item].item for item in self._nodes)
     """
@@ -35,3 +37,100 @@ class _Node:
     def __init__(self) -> None:
         """Initialize an empty graph (no vertices or edges)."""
         self._nodes = {}
+
+    def add_node(self, item: Any):
+        """Add a vertex with the given item to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+
+        Preconditions:
+            - item not in self._vertices
+        """
+        self._nodes[item] = _Node(item, set())
+
+    def add_connection(self, item1: Any, item2: Any) -> None:
+        """Add a connection between the two nodes with the given items in this graph.
+
+        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - item1 != item2
+        """
+        if item1 in self._nodes and item2 in self._nodes:
+            n1 = self._nodes[item1]
+            n2 = self._nodes[item2]
+
+            # Add the new edge
+            n1.neighbours.add(n2)
+            n2.neighbours.add(n1)
+        else:
+            # We didn't find an existing vertex for both items.
+            raise ValueError
+
+
+class Character(_Node):
+    """A character node in the graph.
+
+    Instance Attributes:
+        - name: The name of the character.
+        - traits: List of character traits.
+        - motive: Character's motive or goal.
+        - relationships: Dictionary of character relationships.
+    """
+    name: str
+    traits: list[str]
+    motive: str
+    relationships: dict[str, str]
+
+    def __init__(self, name: str, traits: list[str], motive: str) -> None:
+        """Initialize a character node with the given attributes."""
+        super().__init__({'name': name, 'traits': traits, 'motive': motive, 'relationships': {}})
+
+    def add_relationship(self, character: Character, relationship_type: str) -> None:
+        """Add a relationship to another character."""
+        self.item['relationships'][character.name] = relationship_type
+        character.item['relationships'][self.name] = relationship_type
+
+
+class Setting(_Node):
+    """
+    This is a subclass of _Node that stores information on a specific setting from some fairytale.
+    """
+
+    def __init__(self):
+        """"""
+        super().__init__(item=dict, neighbours=set())
+
+
+class Object(_Node):
+    """
+    This is a subclass of _Node that stores information on a specific object from some fairytale.
+    """
+
+    def __init__(self):
+        """"""
+        super().__init__(item=dict, neighbours=set())
+
+
+class TaleGraph(Graph):
+    """
+    This is a subclass of Graph that stores information on all characters, objects, and settings in a tale.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def add_character(self, name: str, traits: list[str], motive: str, relationships: dict[str, _Node]):
+        """ Add a character as a _Node in the graph.
+        """
+        self.add_node({"name": name, "traits": traits, "motive": motive, })
+
+    def add_setting(self, name: str, description: str):
+        """ Add a setting as a _Node in the graph.
+        """
+        self.add_node({"name": name, "description": description})
+
+    def add_object(self, name: str, description: str):
+        """ Add an object as a _Node in the graph.
+        """
+        self.add_node({"name": name, "description": description})
